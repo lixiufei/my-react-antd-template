c1bbf4be072921485e358dac83be82620fdd901f
lixiufei <li_xiu_fei@163.com>
2019-10-23 16:37:03 +0800

    router

---
+++ src/pages/Home/index.js
@@ -0,0 +1,21 @@
+/**
+ * @name Home
+ * @desc 首页
+ * @author
+ * @version
+ */
+
+import React, { Component } from 'react'
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Home</h1>
+                <p>msg: aa</p>
+            </div>
+        )
+    }
+}
+
+export default Home
